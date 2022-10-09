8553b58d46b13f3761822f24deb8aa2f7c5db08b
Harsh Chougule <harshchougule177@gmail.com>
2022-10-09 18:24:13 +0530

    ADDED: third commit

--- src/main/java/com/sample/SampleJenkinsProjectApplication.java
+++ src/main/java/com/sample/SampleJenkinsProjectApplication.java
@@ -1,14 +1,24 @@
 package com.sample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SampleJenkinsProjectApplication {
 
+    public static Logger logger = LoggerFactory.getLogger(SampleJenkinsProjectApplication.class);
+
+    @PostConstruct
+    public void init() {
+        logger.info("Application started...");
+    }
     public static void main(String[] args) {
+        logger.info("Application executed...");
         SpringApplication.run(SampleJenkinsProjectApplication.class, args);
-        System.out.println("Sample project running successfully...");
     }
 
 }
--- src/test/java/com/sample/SampleJenkinsProjectApplicationTests.java
+++ src/test/java/com/sample/SampleJenkinsProjectApplicationTests.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.PostConstruct;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
