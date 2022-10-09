8fcbe1bece64ec109738eb689a2c751f55b0ffeb
Harsh Chougule <harshchougule177@gmail.com>
2022-10-09 18:16:00 +0530

    ADDED: second commit

--- src/test/java/com/sample/SampleJenkinsProjectApplicationTests.java
+++ src/test/java/com/sample/SampleJenkinsProjectApplicationTests.java
@@ -1,13 +1,25 @@
 package com.sample;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SampleJenkinsProjectApplicationTests {
+public class SampleJenkinsProjectApplicationTests {
+
+    public static Logger logger = LoggerFactory.getLogger(SampleJenkinsProjectApplicationTests.class);
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        logger.info("Test case executing...");
+        assertEquals(true, true);
     }
 
 }
